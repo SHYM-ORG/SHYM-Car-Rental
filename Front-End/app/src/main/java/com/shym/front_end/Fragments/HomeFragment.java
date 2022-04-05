e213f6f30c02442c0b103a6b7d9f9d8702bd3a77
karami4yasser <83478271+karami4yasser@users.noreply.github.com>
2022-04-05 18:06:18 +0000

    V1

---
+++ Front-End/app/src/main/java/com/shym/front_end/Fragments/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.shym.front_end.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.shym.front_end.R;
+import com.shym.front_end.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+
+}
